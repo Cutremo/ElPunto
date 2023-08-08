57dc4c23c6b140c43c63ae120ea64446fcb5e292
Ángel Siendones Sillero <culoextremo@gmail.com>
2023-08-08 13:57:03 +0200

    feat: add audios

--- Assets/Submission.cs
+++ Assets/Submission.cs
@@ -9,4 +9,11 @@ public class Submission
     public Color color;
     public AudioClip sound;
     public float volume = 1;
+
+    public Submission(AudioClip sound, Color color, int volume)
+    {
+        this.sound = sound;
+        this.color = color;
+        this.volume = volume;
+    }
 }
--- Assets/Survey.cs
+++ Assets/Survey.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Survey")]
 public class Survey : ScriptableObject, IEnumerable<Submission>
 {
+    [SerializeField] List<AudioClip> clips;
     [SerializeField] List<Submission> submissions;
     public IEnumerator<Submission> GetEnumerator()
     {
@@ -14,5 +17,18 @@ public class Survey : ScriptableObject, IEnumerable<Submission>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+
+    void OnValidate()
+    {
+        foreach(var clip in clips)
+        {
+            if(submissions.All(x => x.sound != clip))
+            {
+                submissions.Add(new Submission(clip, Color.black, 1));
+            }
+        }
+
+        submissions = submissions.Where(x => x.sound != null).ToList();
     }
 }
